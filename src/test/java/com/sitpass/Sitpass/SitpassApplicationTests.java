24e40a495707935a32293f48fe5397617657c146
Sinisa Batinic <sinisabatinic5@gmail.com>
2024-08-02 20:58:30 +0200

    Project setup

---
+++ src/test/java/com/sitpass/Sitpass/SitpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitpass.Sitpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
